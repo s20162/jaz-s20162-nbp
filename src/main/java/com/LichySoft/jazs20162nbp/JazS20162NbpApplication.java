ceb88ab80b99ba36c958df6250f2f0eca5c8b7a0
adam_lichy <s20162@pjwstk.edu.pl>
2021-07-03 16:09:03 +0200

    Initial commit

---
+++ src/main/java/com/LichySoft/jazs20162nbp/JazS20162NbpApplication.java
@@ -0,0 +1,13 @@
+package com.LichySoft.jazs20162nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20162NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20162NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LichySoft/jazs20162nbp/JazS20162NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LichySoft.jazs20162nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20162NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
